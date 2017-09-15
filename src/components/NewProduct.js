79ebb0fd99b949f84bfcefa5f76013ce85e89c7d
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-09-15 13:58:18 -0600

    Camera Roll Connected

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -1,15 +1,61 @@
-import React from 'react';
-import {View, Text, StyleSheet, Platform} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, Image, StyleSheet, Platform, CameraRoll} from 'react-native';
 
-import {Header} from './common';
+import {Header, CardSection, Button} from './common';
 
-const NewProduct = props => {
-  return(
-    <View style={styles.container}>
-      <Header isBackProp={false}/>
-      <Text>New Product</Text>
-    </View>
-  );
+
+class NewProduct extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      images: []
+    }
+    this.getPhotos = this.getPhotos.bind(this);
+  }
+
+  getPhotos() {
+    CameraRoll.getPhotos({
+      first: 20,
+    })
+    .then(response => {
+        const assets = response.edges;
+        const images = assets.map(asset => {
+          return asset.node.image
+        });
+        this.setState({
+          isLoaded: true,
+          images
+        });
+    }).catch(error => {
+      console.error(error);
+    });
+  }
+
+  renderImages() {
+    if (this.state.isLoaded) {
+      return this.state.images.map(image => {
+        console.log(image);
+        return <Image key={image.filename} source={{uri: image.uri}} style={styles.images}/>
+      });
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header isBackProp={false}/>
+        <View>
+          {this.renderImages()}
+        </View>
+        <CardSection>
+          <Button onPress={this.getPhotos} color="#D62246">
+            ADD
+          </Button>
+        </CardSection>
+      </View>
+    );
+  }
 }
 
 export default NewProduct;
@@ -17,5 +63,9 @@ export default NewProduct;
 const styles = StyleSheet.create({
   container: {
     marginTop: Platform.OS === 'ios' ? 20 : 0
+  },
+  images: {
+    width: 50,
+    height: 50
   }
 });
